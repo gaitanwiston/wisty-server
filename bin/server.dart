cf2ff9d91cb08b49543757dc108d646fa8db0d35
gaitanwiston <gaitanwiston@gmail.com>
2026-03-12 14:22:42 +0300

    Fix router structure for Dart Frog server

--- bin/server.dart
+++ bin/server.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
-import 'package:wisty_server/routes/_middleware.dart';
-import 'package:wisty_server/routes/_router.dart';
 
 Future<void> main() async {
   final handler = const Pipeline()
       .addMiddleware(logRequests())
-      .addHandler(router);
+      .addHandler(_router);
 
   final ip = InternetAddress.anyIPv4;
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
---
+++ lib/router.dart
@@ -0,0 +1,4 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'routes/_router.dart' as generated;
+
+Handler get _router => generated.router;
