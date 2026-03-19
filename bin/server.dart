7fb93305bed5df5221b41c281b8c3a2deabf1ec6
gaitanwiston <gaitanwiston@gmail.com>
2026-03-19 11:57:16 +0300

    Initial Dart Frog server setup with routes

--- bin/server.dart
+++ bin/server.dart
@@ -4,7 +4,7 @@ import 'package:dart_frog/dart_frog.dart';
 // ---------------- HTTP Route Handlers ----------------
 import '../routes/trades.dart' as trades;
 import '../routes/pairs.dart' as pairs;
-import '../routes/last-prices.dart' as last_prices; // underscore instead of hyphen
+import '../routes/last_prices.dart' as last_prices; // underscore instead of hyphen
 import '../routes/candles.dart' as candles;
 import '../routes/balance.dart' as balance;
 
@@ -13,7 +13,7 @@ Future<void> main() async {
   final router = Router()
     ..all('/trades', trades.onRequest)
     ..all('/pairs', pairs.onRequest)
-    ..all('/last-prices', last_prices.onRequest) // corrected import
+    ..all('/last_prices', last_prices.onRequest) // corrected import
     ..all('/candles', candles.onRequest)
     ..all('/balance', balance.onRequest);
 
